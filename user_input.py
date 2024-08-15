1f2af53b9ff5949174aacb11a174064838b59044
mr-momanyi <justtinmomanyi@gmail.com>
2024-08-15 20:28:34 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,12 @@
+Python 3.13.0a4 (tags/v3.13.0a4:9d34f60, Feb 15 2024, 17:00:30) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = input("enter your name: ")
+enter your name: Justine
+>>> age = input("enter your age: ")
+enter your age: 23
+>>> location = input("please enter your location: ")
+please enter your location: Nakuru
+>>> print("Hello {name}, you are {age} years old and live in {location}.")
+Hello {name}, you are {age} years old and live in {location}.
+>>> print(f"Hello {name}, you are {age} years old and live in {location}.")
+Hello Justine, you are 23 years old and live in Nakuru.
